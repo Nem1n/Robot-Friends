9574813d91edfdbfcd54b8ed2aa39750c2b5bbd7
Nem1n <nemineali@gmail.com>
2019-06-12 13:23:49 +0200

    initialisation

---
+++ robofriends/src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+  	id:1,
+  	name:'Junior Paz',
+  	username:'Junio',
+  	email:'junio@gmail.com'
+  },
+  {
+  	id:2,
+  	name:'Chistophe Sery',
+  	username:'Kriss-Kross',
+  	email:'kriss@gmail.com'
+  },
+  {
+  	id:3,
+  	name:'Kevin Monges',
+  	username:'Kvin',
+  	email:'kvin@gmail.com'
+  },
+  {
+  	id:4,
+  	name:'Nadjil Housseni',
+  	username:'Nch',
+  	email:'nch@gmail.com'
+  },
+  {
+  	id:5,
+  	name:'Abdel Mejoun',
+  	username:'Abd',
+  	email:'abd@gmail.com'
+  },
+  {
+  	id:6,
+  	name:'Saidou Diallo',
+  	username:'Dousai',
+  	email:'dousai@gmail.com'
+  },
+  {
+  	id:7,
+  	name:'Kelmine ALI',
+  	username:'Kelmin',
+  	email:'klm@gmail.com'
+  },
+  {
+  	id:8,
+  	name:'Soline Schwartz',
+  	username:'Soso',
+  	email:'sos@gmail.com'
+  },
+];
